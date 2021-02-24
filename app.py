b240207fa852ca942e4fc9b4ebadbddafe8d660a
Laney Light <laneylight@gmail.com>
2021-02-24 13:09:29 -0500

    first draft: rule based transformation
    randomly choose rotation, shift, or reflection

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, json
 from flask_cors import CORS, cross_origin
+#from scipy.stats import bernoulli
+import random
+import numpy as np
 
 api = Flask(__name__)
 cors = CORS(api)
@@ -10,16 +13,36 @@ def reactToDraw():
   width = request.get_json().get("width")
   height = request.get_json().get("height")
 
+  #rotate = bernoulli.rvs(p=0.5, size=1)
+  rotateOptions = [90, 180, 270]
+  rotateDeg = random.choice(rotateOptions)
+  transOptions=['shift','rotate','reflect']
+  transformation=random.choice(transOptions)
+
   responseTurn = []
   for stroke in strokes:
     responseStroke = []
+    midstroke=stroke[round(len(stroke)/2)-1]
+    midX = midstroke[0]
+    midY = midstroke[1]
+    xshift=random.randint(-50,50)
+    yshift=random.randint(-50,50)
     for point in stroke:
-      responseX = width - point[0]
-      responseY = height - point[1]
-      responseStroke.append([responseX, responseY])
+        xin=point[0]
+        yin=point[1]
+        if transformation=='reflect':
+            responseX = width - xin
+            responseY = height - yin
+        elif transformation=='rotate':
+            responseX = np.cos(rotateDeg) * (xin - midX) - np.sin(rotateDeg) * (yin - midY) + midX
+            responseY = np.sin(rotateDeg) * (xin - midX) + np.cos(rotateDeg) * (yin - midY) + midY
+        elif transformation=='shift':
+            responseX = xin + xshift
+            responseY = yin + yshift
+        responseStroke.append([responseX, responseY])
     responseTurn.append(responseStroke)
 
   return json.dumps({"data": responseTurn})
 
 if __name__ == '__main__':
-    api.run()
+    api.run(port=8000)
