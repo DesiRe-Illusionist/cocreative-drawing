1623c8e6cfffaeb6bd491f359ac510ff959f7853
MaD171093 <md171093@gmail.com>
2021-03-02 12:31:16 -0500

    Sketch RNN agent

--- sketch.js
+++ sketch.js
@@ -1,99 +1,184 @@
-let button;
-let curTurn;
-let curStroke;
-let aiTurn;
+let clearButton;
+let sketchRNN;
+let currentStroke;
+let x, y;
+let nextPen = 'down';
+let seedPath = [];
+let seedPoints = [];
+let personDrawing = false;
+let AiStroke;
+
+let ctr =0;
+
+
+function preload() {
+  sketchRNN = ml5.sketchRNN('everything');
+}
+
 
 function setup() {
 	let myCanvas = createCanvas(600, 600);
 	myCanvas.parent("canvas");
 
-	button = createButton('CLEAR');
-	button.center();
-	button.style('margin-top', '45vh');
-	button.mousePressed(() => {
+	myCanvas.mousePressed(startDrawing);
+  myCanvas.mouseReleased(sketchRNNStart);
+
+	clearButton = createButton('CLEAR');
+	clearButton.center();
+	clearButton.style('margin-top', '45vh');
+	clearButton.mousePressed(() => {
 		background(255);
 	});
 
 	background(255);
 
 	currentSetting = {
-		size: 20,
-		col: color(0, 0, 0, 150)
+		size: 2,
+		col: color(0, 0, 0)
 	}
 
 	agentSetting = {
-		size: 20,
-		col: color(150, 0, 0, 150)
+		size: 2,
+		col: color(150, 0, 0)
 	}
 
-	curTurn = [];
-	curStroke = [];
-	aiTurn = [];
+}
+
+
+function startDrawing() {
+  personDrawing = true;
+  x = mouseX;
+  y = mouseY;
+}
+
+function sketchRNNStart() {
+  personDrawing = false;
+
+  // Perform RDP Line Simplication
+  const rdpPoints = [];
+  const total = seedPoints.length;
+  const start = seedPoints[0];
+  const end = seedPoints[total - 1];
+  rdpPoints.push(start);
+  rdp(0, total - 1, seedPoints, rdpPoints);
+  rdpPoints.push(end);
+
+  // Drawing simplified path
+  background(255,5);
+  //stroke(agentSetting.col);
+  //strokeWeight(agentSetting.size);
+  beginShape();
+	let c = color(random(0,255),random(0,255),random(0,255));
+	c.setAlpha(50);
+	fill(c);
+  //noFill();
+  for (let v of rdpPoints) {
+    vertex(v.x, v.y);
+  }
+  endShape();
+
+  x = rdpPoints[rdpPoints.length - 1].x;
+  y = rdpPoints[rdpPoints.length - 1].y;
+
+  seedPath = [];
+  // Converting to SketchRNN states
+  for (let i = 1; i < rdpPoints.length; i++) {
+    let strokePath = {
+      dx: rdpPoints[i].x - rdpPoints[i - 1].x,
+      dy: rdpPoints[i].y - rdpPoints[i - 1].y,
+      pen: 'down'
+    };
+    seedPath.push(strokePath);
+  }
+
+  sketchRNN.generate(seedPath, gotStrokePath);
+}
+
+
+function gotStrokePath(error, strokePath) {
+  currentStroke = strokePath;
 }
 
 function draw() {
+	//stroke(0);
+ 	//strokeWeight(4);
 
-	if (aiTurn.length == 0) {
-		if (mouseIsPressed && isInsideCanvas(mouseX, mouseY)) {
-			stroke(currentSetting.col);
-			noFill();
-			strokeWeight(currentSetting.size);
-			line(pmouseX, pmouseY, mouseX, mouseY);
-			curStroke.push([mouseX, mouseY]);
-		}
-	} else {
+	if (personDrawing) {
+		stroke(currentSetting.col);
+	  strokeWeight(currentSetting.size);
+		line(mouseX, mouseY, pmouseX, pmouseY);
+		seedPoints.push(createVector(mouseX, mouseY));
+	 }
+
+	if (currentStroke) {
 		stroke(agentSetting.col);
-		noFill();
-		strokeWeight(agentSetting.size);
+	  strokeWeight(agentSetting.size);
 
-		for (var i = 0; i < aiTurn.length; i++) {
-			if (aiTurn[i].length > 1) {
-				for (var j = 1; j < aiTurn[i].length; j++) {
-					console.log(aiTurn[i][j-1][0].toString(), aiTurn[i][j-1][1].toString(), aiTurn[i][j][0].toString(), aiTurn[i][j][1].toString())
-					line(aiTurn[i][j-1][0], aiTurn[i][j-1][1], aiTurn[i][j][0], aiTurn[i][j][1])
-				}
-			}
+		let noOfStroke = random (2,10);
+		if (ctr < noOfStroke){
+			if (nextPen == 'end') {
+	 		 //sketchRNN.reset();
+	 		 //sketchRNNStart();
+	 		 currentStroke = null;
+	 		 noLoop();
+	 		 nextPen = 'down';
+	 		 return;
+	 	 }
+
+	 	if (nextPen == 'down') {
+			line(x, y, x + currentStroke.dx, y + currentStroke.dy);
+	 	}
+	 		x += currentStroke.dx;
+	 		y += currentStroke.dy;
+	 		nextPen = currentStroke.pen;
+	 		currentStroke = null;
+	 		sketchRNN.generate(gotStrokePath);
+
+			ctr += 1;
+		}
+		else{
+			//personDrawing = true;
+			seedPoints = [];
+			ctr = 0;
+			AiStroke = currentStroke;
+			currentStroke = null;
+			sketchRNN.reset();
 		}
 
-		aiTurn = [];
-	}
-}
 
-function mouseReleased() {
-	if (curStroke.length > 0) {
-		curTurn.push(curStroke);
-		curStroke = [];
-	}
-}
 
-function isInsideCanvas(x, y) {
-	if (x >= 0 && x <= width && y >= 0 && y <= height) {
-		return true;
-	} else {
-		return false;
-	}
-}
-
-finishTurn = () => {
-
-	console.log(curTurn);
-	playerTurn = false;
-
-	let postData = {
-		"data" : curTurn,
-		"width" : width,
-		"height" : height
 	}
 
-	httpPost(
-		"http://127.0.0.1:8000/draw", 
-		"json", 
-		postData,
-		(response) => {
-			aiTurn = response.data;
-		});
-  }
-
-changeColor = newCol => {
-	col = newCol;
 }
+
+// function isInsideCanvas(x, y) {
+// 	if (x >= 0 && x <= width && y >= 0 && y <= height) {
+// 		return true;
+// 	} else {
+// 		return false;
+// 	}
+// }
+//
+// finishTurn = () => {
+//
+// 	console.log(curTurn);
+// 	playerTurn = false;
+//
+// 	let postData = {
+// 		"data" : curTurn,
+// 		"width" : width,
+// 		"height" : height
+// 	}
+//
+// 	httpPost(
+// 		"http://127.0.0.1:8000/draw",
+// 		"json",
+// 		postData,
+// 		(response) => {
+// 			aiTurn = response.data;
+// 		});
+//   }
+//
+// changeColor = newCol => {
+// 	col = newCol;
+// }
